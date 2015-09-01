afdfee35ef258f2af006466614bec99b0e8b72bf
handikacp <handikacahyo@gmail.com>
2015-09-01 15:20:33 +0700

    latihan 1

---
+++ latihan 1.py
@@ -0,0 +1,4 @@
+print "Selamat Datang"
+nama = raw_input ("Masukkan Nama Anda : ")
+umur = input ("Masukkan Umur Anda : ")
+print "Halo {0}, umur Anda {1}".format(nama,umur)
---
+++ luas segitiga.py
@@ -0,0 +1,3 @@
+A = input (" Input Alas : ")
+T = input ("input tinggi : ")
+print "Luas Segitiga adalah : {0}".format(A*T/2)
